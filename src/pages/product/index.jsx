99645f0f113b9470cc0cf84338123f5306b7c4ec
TienDuong <duongb2012073@gmail.com>
2024-10-01 14:03:10 +0700

    update

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import ViewDetail from "../../components/Book/ViewDetail";
+import ViewDetail from "../../components/Product/ViewDetail";
 import { callFetchBookById } from "../../services/api";
 
 const ProductPage = () => {
